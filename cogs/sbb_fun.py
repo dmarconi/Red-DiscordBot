9e484129e744ae960e3895cfd607d5283d828f00
Daniele Marconi <daniele.marconi@desy.de>
2017-03-08 17:06:24 +0100

    Added Insults

--- cogs/sbb_fun.py
+++ cogs/sbb_fun.py
@@ -1,6 +1,9 @@
 import discord
-import random
 from discord.ext import commands
+import chardet
+import os
+import random
+from random import shuffle, choice
 
 class SbbFun:
     """SBB-fun-shaped commands!"""
@@ -8,12 +11,24 @@ class SbbFun:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def insultpa(self):
-        """Insulta pA!!"""
+    def guess_encoding(self, trivia_list):
+        with open(trivia_list, "rb") as f:
+            try:
+                return chardet.detect(f.read())["encoding"]
+            except:
+                return "ISO-8859-1"
+
+    @commands.command(pass_context=True)
+    async def insult(self, ctx, user : discord.Member=None):
+        """Insulta utente"""
 
         #Your code will go here
-        await self.bot.say(":poop: Pape merda :poop:")
+        ifile = 'data/sbb/insults.txt'
+        encoding = self.guess_encoding(ifile)
+        lines = open(ifile, "r", encoding=encoding).read().splitlines()
+        line =random.choice(lines)
+
+        await self.bot.say(user.mention + line)
 
     @commands.command()
     async def punch(self, user : discord.Member):
@@ -25,8 +40,7 @@ class SbbFun:
 
     @commands.command(pass_context=True)
     async def ribaltone(self, ctx, user : discord.Member=None):
-        """ Ribalta un utente
-        """
+        """Ribalta un utente"""
         if user != None:
             msg = ""
             if user.id == self.bot.user.id:
@@ -40,10 +54,17 @@ class SbbFun:
             tran = "∀qƆpƎℲפHIſʞ˥WNOԀQᴚS┴∩ΛMX⅄Z"
             table = str.maketrans(char, tran)
             name = name.translate(table)
-            await self.bot.say(msg + "(╯°□°）╯︵ " + name[::-1])
+            await self.bot.say(msg + "(╯ °□° ）╯︵ " + name[::-1])
         else:
             await self.bot.say("Devi scegliere qualcuno tund!")
 
+    @commands.command(pass_context=True, no_pm=True)
+    async def singon(self, ctx):
+        """Makes Red sing one of her songs"""
+        ids = ("zGTkAVsrfg8", "cGMWL8cOeAU", "vFrjMq4aL-g", "WROI5WYBU_A",
+               "41tIUr_ex3g", "f9O2Rjn1azc")
+        url = "https://www.youtube.com/watch?v={}".format(choice(ids))
+        await ctx.invoke(self.play, url_or_search_terms=url)
 
 def setup(bot):
     bot.add_cog(SbbFun(bot))
